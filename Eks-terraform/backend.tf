b013e190986c254036ddfc34154b4d9e5d1b4fa7
hemanth-vurimi <80247546+hemanth-vurimi@users.noreply.github.com>
2024-05-30 19:16:48 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hemanth-eks-project-1210" # Replace with your actual S3 bucket name
+    bucket = "hemanth-s3-terraform-1210" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
